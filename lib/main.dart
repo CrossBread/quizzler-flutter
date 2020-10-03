072c631b5e1e1f64d269e2a0102b57ac125db176
Jeff <jeff.neet@gmail.com>
2020-10-03 17:48:07 -0600

    Extracted biz logic to Quiz Brain and questions to Questions class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,9 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionNumber = 0;
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +43,10 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain
+                    .questionsBank[
+                        questionNumber % quizBrain.questionsBank.length]
+                    .questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +70,19 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain
+                    .questionsBank[
+                        questionNumber % quizBrain.questionsBank.length]
+                    .questionAnswer;
+                if (correctAnswer == true) {
+                  print('Correct!');
+                } else {
+                  print('Wrong!');
+                }
                 //The user picked true.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -79,12 +100,26 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = quizBrain
+                    .questionsBank[
+                        questionNumber % quizBrain.questionsBank.length]
+                    .questionAnswer;
+                if (correctAnswer == false) {
+                  print('Correct!');
+                } else {
+                  print('Wrong!');
+                }
+                //The user picked true.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
@@ -92,6 +127,6 @@ class _QuizPageState extends State<QuizPage> {
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+question2: , true,
+question3: , true,
 */
